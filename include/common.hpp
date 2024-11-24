e0c3229a3d58fe47cd252a086ea2b5225338c39a
NunesAzevedo <pnazevedo1@gmail.com>
2024-11-23 21:11:34 -0300

    a

--- include/common.hpp
+++ include/common.hpp
@@ -2,9 +2,10 @@
     Header de uso comum para as funções do projeto
 */
 
+#include <string>
 
-#ifndef COMMON_HPP
-#define COMMON_HPP
+#ifndef COMMON_H
+#define COMMON_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -19,23 +20,27 @@ using namespace std;
 #define TAM_DISCO 1600
 #define TAM_DADOS_FIXOS 18
 #define T_MAX 50
+#define branco 0
+#define cinza  1
+#define preto 2
+#define vermelho 3
+#define INF 1410065407
 
-typedef struct
-{
+typedef struct {
     int populacao;
     char alimento[T_MAX];
 } noListaAdj;
 
-typedef struct
-{
+typedef struct {
     char nome[T_MAX];
     string especie;
     string habitat;
     string tipo;
     string dieta;
-    list<noListaAdj> lista;
+    list <noListaAdj> lista;
     int grauSaida;
     int grauEntrada;
+    int cor;
 } SVertice;
 
 
